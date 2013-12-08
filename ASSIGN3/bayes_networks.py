804634698f92ffcf096beda6e677608712a3d7c0
chiroptera <chiroptera@dove.(none)>
2013-12-08 15:39:16 +0000

    DONE: - CPT parsing done. - Every node is saved in a dicitonary (Bayes network representation).

--- ASSIGN3/bayes_networks.py
+++ ASSIGN3/bayes_networks.py
@@ -1,16 +1,19 @@
 class bayesVar:
 
-    def __init__(self,name):
+    def __init__(self):
         self.name = False
         self.alias = False
-        self.parents = []
+        self.parentsNames = []
         self.values = False
+
+    def setName(self,name):
+        self.name = name
 
     def setAlias(self,alias):
         self.alias = alias
 
     def setParents(self,parents):
-        self.parents=list(parents)
+        self.parentsNames=list(parents)
 
     def setValues(self,values):
         self.values = list(values)
@@ -19,13 +22,20 @@ class bayesVar:
         self.CPT = CPT
 
     def Print(self):
-        for f in [self.name,self.alias,self.parents,self.values]:
-            if f is not False and f is not []:
-                print f
+        print 'Name: ',self.name
+        if self.alias: print 'Alias: ', self.alias
+        if self.parentsNames != []:
+            print 'Parents: ', self.parentsNames
+        print 'Values: ', self.values
+        print 'CPT'
+        for key,value in self.CPT.iteritems():
+            print key,value
 
+    def connectToParents(self,setOfNodes):
+        if self.parentsNames != []:
+            self.parents=list()
+            for parent in self.parentsNames:
+                self.parents.append(setOfNodes[parent])
 
-class CPT:
-    pass
-
-class network:
-    pass
+    def getNumParents(self):
+        return len(self.parentsNames)
--- ASSIGN3/parse.py
+++ ASSIGN3/parse.py
@@ -23,13 +23,18 @@ def parseBN(input):
 
     while(line != len(input)):
 
+        # parse variable
         if len(input[line]) > 0 and input[line][0] != '#' and 'VAR' in input[line]:
-            var = bayesVar(input[line+1].split(" ")[1])
-            varline = line+2
+            var = bayesVar()
+
+            varline = line+1
 
             while(input[varline] != ''):
 
-                if 'alias' in input[varline]:
+                if 'name' in input[varline]:
+                    var.setName(input[line+1].split(" ")[1])
+
+                elif 'alias' in input[varline]:
                     var.setAlias(input[varline].split(" ")[1])
 
                 elif 'parents' in input[varline]:
@@ -44,35 +49,63 @@ def parseBN(input):
 
                 varline += 1
 
-            print var.Print()
-
+            # add node to bayes network with both name and alias as keys
             listOfVars[var.name] = var
             if var.alias is not False:
                 listOfVars[var.alias] = var
 
+        line += 1
+
+    line = 0
+    while(line != len(input)):
+        # parse Conditional Probability Table (CPT)
         if len(input[line]) > 0 and input[line][0] != '#' and 'CPT' in input[line]:
-            print input[line+1]
-            variable = input[line+1].split(" ")[1]
-            variable = listOfVars[variable]
+
+            variable = input[line+1].split(" ")[1] # line after CPT names variable
+            variable = listOfVars[variable]        # gets bayes node from list
+
+            # connect node to parents
+            variable.connectToParents(listOfVars)
+
+            # first value is from variable, the rest from parents
+            numberOfValues = 1 + variable.getNumParents()
+
+            # CPT is represented as a dictionary
+            # the key is a tuple with a value combination
+            # the key's value is the probability
+            cpt=dict()
 
             # the table is in one line
             if 'table' in input[line+2] and len('table') < len(input[line+2]):
                 table = input[line+2].split(" ")
-                table.pop(0)
-
-                # first value is from variable, the rest from parents
-                numberOfValues = 1 + len(variable.parents)
-
-                cpt=dict()
+                table.pop(0)    # remove 'table' sting
 
                 while(len(table) != 0):
                     values=list()
+                    # get combination of values
                     for it in range(0,numberOfValues):
                         values.append(table.pop(0))
+                    # tuple from value combination as key, probability as dictionary value
                     cpt[tuple(values)] = float(table.pop(0))
-                    print cpt
 
+            elif 'table' in input[line+2] and len('table') == len(input[line+2]):
+
+                cptLine = line + 3 # line iterator
+                while(input[cptLine] != ''):
+                    values=list()
+                    table = input[cptLine].split(" ")
+
+                    for it in range(0,numberOfValues):
+                        values.append(table.pop(0))
+
+                    cpt[tuple(values)] = float(table.pop(0))
+                    cptLine += 1
+
+            variable.setCPT(cpt)
+            print variable.CPT
         line += 1
+
+    return listOfVars
 
 
 if len(sys.argv) == 2:
@@ -88,4 +121,8 @@ if stuff != -1:
     for f in stuff:
         print f
 
-    parseBN(stuff)
+    bayesNetwork = parseBN(stuff)
+
+    for (key,value) in bayesNetwork.iteritems():
+        print '---------------------------'
+        value.Print()
